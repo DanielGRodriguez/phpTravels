f52f9e5e30aa15626d62353a726bccc8ef74fed8
[DanielGRodriguez] <[drodriguez@solvd.com]>
2022-04-18 18:55:43 -0300

    2.1-Phptravels

--- src/main/java/com/qaprosoft/carina/demo/phpTravels/components/BookingsButtons.java
+++ src/main/java/com/qaprosoft/carina/demo/phpTravels/components/BookingsButtons.java
@@ -25,6 +25,21 @@ public class BookingsButtons extends AbstractUIObject{
     @FindBy(xpath = "//div[@class='row gx-3']/div[6]")
     private ExtendedWebElement buttonRefunded;
 
+    @FindBy(xpath = "//td/select[@id='booking_status']")
+    private ExtendedWebElement tableBookingStatus;
+
+    @FindBy(xpath = "//td/select[@id='payment_status']")
+    private ExtendedWebElement tablePaymentStatus;
+
+    @FindBy(xpath = "//td/a")
+    private ExtendedWebElement tablePaymentExecute;
+
+    @FindBy(xpath = "//td/a[@target='_blank']")
+    private ExtendedWebElement buttonInvoice;
+
+    @FindBy(xpath = "//td/button[@class='btn btn-danger mdc-ripple-upgraded']")
+    private ExtendedWebElement buttonDeleteOperation;
+
     public BookingsButtons(WebDriver driver) {
         super(driver);
     }
